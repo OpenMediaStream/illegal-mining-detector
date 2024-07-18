b139ebdfdc0390fedcc51f5f815005dc58f064de
Parelho <vinicius.parelho@outlook.com>
2024-07-18 12:23:58 -0300

    more tests

--- python/predict.py
+++ python/predict.py
@@ -4,7 +4,7 @@ from ultralytics import YOLO
 
 model = YOLO("./runs/detect/Sentinel+Landsat/weights/best.pt")
 
-results = model("./teste.jpg", iou=0.7, conf=0.4)
+results = model("./teste3.jpg", iou=0.4, conf=0.3)
 print(results)
 
 for i, r in enumerate(results):
